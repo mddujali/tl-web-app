f10656ff2a0adb2639bd490df17ffc3a06014685
Mark Dujali <mddujali@gmail.com>
2025-07-03 20:39:40 +0800

    Add Bootstrap and Font Awesome

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
 import './assets/main.css'
 
 import { createApp } from 'vue'
